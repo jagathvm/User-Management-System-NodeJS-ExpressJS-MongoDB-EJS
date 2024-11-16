090f9a70d4522ecffc2e95ad02d29cb77fc8c88d
JagathVM <17jagathvm@gmail.com>
2024-11-17 01:17:50 +0530

    Refactor: reused HttpRequest class in editUser script

--- public/scripts/user/editUser.js
+++ public/scripts/user/editUser.js
@@ -1,52 +1,57 @@
+import { apiClient } from "../apiServices/httpRequest.js";
+
 const updateUserForm = document.getElementById("updateUserForm");
 const deleteUserForm = document.getElementById("deleteUserForm");
 const username = document.getElementById("username").value;
 
+const handleRedirect = (message, url) => {
+  alert(message);
+  window.location.href = url;
+};
+
 // Fetch data from updateUser Form
 updateUserForm.addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  // Extract data from the form
   const formData = new FormData(updateUserForm);
   const data = Object.fromEntries(formData);
 
+  // Client-side validation
+  if (!data.username || !data.email || !data.tel || !data.role) {
+    alert("All fields are required.");
+    return;
+  }
+
   try {
-    const result = await fetch(`/api/admin/dashboard/view/update/${username}`, {
-      method: "PUT",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data),
-    });
-    const response = await result.json();
+    const result = await apiClient.httpRequest(
+      `/admin/dashboard/view/update/${username}`,
+      "PUT",
+      data
+    );
 
-    if (!response.success) {
-      alert(response.message);
-    }
-    alert(response.message);
-
-    window.location.href = `/api/admin/dashboard/view/${response.data}`;
+    handleRedirect(result.message, `/api/admin/dashboard/view/${username}`);
   } catch (error) {
-    console.error(error);
+    console.error("Error in updating user:", error);
+    alert("An error occurred during user update. Please try again later.");
+    throw error;
   }
 });
 
 // Delete user data when submit event is triggered
 deleteUserForm.addEventListener("submit", async (e) => {
   e.preventDefault();
+
   try {
-    const result = await fetch(`/api/admin/dashboard/view/delete/${username}`, {
-      method: "DELETE",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-    const response = await result.json();
+    const result = await apiClient.httpRequest(
+      `/admin/dashboard/view/delete/${username}`,
+      "DELETE"
+    );
 
-    if (!response.success) {
-      alert(response.message);
-    }
-    alert(response.message);
-
-    window.location.href = "/api/admin/dashboard";
+    handleRedirect(result.message, "/api/admin/dashboard");
   } catch (error) {
-    console.error(error);
+    console.error("Error in deleting user:", error);
+    alert("An error occurred during user deletion. Please try again later.");
+    throw error;
   }
 });
