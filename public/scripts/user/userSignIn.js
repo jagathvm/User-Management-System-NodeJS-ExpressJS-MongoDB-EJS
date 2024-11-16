f5dbe51a157498d21983c01b7efe8aa8f098af54
JagathVM <17jagathvm@gmail.com>
2024-11-17 00:59:38 +0530

    Refactor: Enhance user sign-in process with validation and feedback

--- public/scripts/user/userSignIn.js
+++ public/scripts/user/userSignIn.js
@@ -4,15 +4,24 @@ const formSignIn = document.getElementById("formSignIn");
 formSignIn.addEventListener("submit", async (e) => {
   e.preventDefault();
 
+  // Extract data from the form
   const formData = new FormData(formSignIn);
   const data = Object.fromEntries(formData);
+
+  // Client-side validation
+  if (!data.username || !data.password) {
+    alert("Username and password are required.");
+    return;
+  }
 
   try {
     const result = await apiClient.httpRequest("/user/", "POST", data);
     const redirectUrl =
       result.data === 1 ? "/api/admin/dashboard" : "/api/user/home";
-
     alert(result.message);
+
+    // Reset form after successful submission
+    formSignIn.reset();
     window.location.href = redirectUrl;
   } catch (error) {
     console.log("Sign-in error: ", error);
