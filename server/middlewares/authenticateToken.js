bd00c7e5de210602f3ea33bc9d41c062bd3a295c
JagathVM <17jagathvm@gmail.com>
2024-11-17 19:43:32 +0530

    Refactor: improve response handling in authenticateToken middleware

--- server/middlewares/authenticateToken.js
+++ server/middlewares/authenticateToken.js
@@ -3,10 +3,12 @@ import jwt from "jsonwebtoken";
 export const authenticateToken = (req, res, next) => {
   // Get the JWT token from the request cookies
   const { accessToken } = req.cookies;
-  if (!accessToken) return res.sendStatus(401);
+  if (!accessToken)
+    return res.status(401).json({ success: false, message: "Unauthorized" });
 
   jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-    if (err) return res.sendStatus(403);
+    if (err)
+      return res.status(403).json({ success: false, message: "Forbidden" });
     req.user = user;
     next();
   });
