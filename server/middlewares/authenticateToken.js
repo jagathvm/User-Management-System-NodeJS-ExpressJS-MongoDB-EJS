3491b4ebd79d0632e024f41af586accbed90d025
JagathVM <17jagathvm@gmail.com>
2024-12-02 19:02:41 +0530

    refactor: remove log messages from authenticateToken middleware

--- server/middlewares/authenticateToken.js
+++ server/middlewares/authenticateToken.js
@@ -4,12 +4,10 @@ export const authenticateToken = (req, res, next) => {
   // Get the JWT token from the request cookies
   const { accessToken } = req.cookies;
   if (!accessToken) {
-    console.log(req.originalUrl);
     if (req.originalUrl.startsWith("/api/user")) {
       console.log("rendering user 404");
       return res.status(404).redirect("/api/user/404");
     }
-    console.log("rendering admin 404");
     return res.render("admin/404", { req });
   }
 
