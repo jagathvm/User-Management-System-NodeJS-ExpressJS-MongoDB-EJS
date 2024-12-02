dc08b60e5cc479955be4a9251bcf23cbc367307c
JagathVM <17jagathvm@gmail.com>
2024-12-02 18:59:17 +0530

    fix: update authenticateToken middleware for better error handling - Modified token validation to render custom 404 pages for both user and admin routes. - Redirects to '/api/user/404' for user-related routes when the token is missing or invalid. - Renders 'admin/404' page for admin routes when the token is missing or invalid.

--- server/middlewares/authenticateToken.js
+++ server/middlewares/authenticateToken.js
@@ -3,12 +3,22 @@ import jwt from "jsonwebtoken";
 export const authenticateToken = (req, res, next) => {
   // Get the JWT token from the request cookies
   const { accessToken } = req.cookies;
-  if (!accessToken)
-    return res.status(401).json({ success: false, message: "Unauthorized" });
+  if (!accessToken) {
+    console.log(req.originalUrl);
+    if (req.originalUrl.startsWith("/api/user")) {
+      console.log("rendering user 404");
+      return res.status(404).redirect("/api/user/404");
+    }
+    console.log("rendering admin 404");
+    return res.render("admin/404", { req });
+  }
 
   jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-    if (err)
-      return res.status(403).json({ success: false, message: "Forbidden" });
+    if (err) {
+      if (req.originalUrl.startsWith("/api/user"))
+        return res.render("user/404", { req });
+      return res.render("admin/404", { req });
+    }
     req.user = user;
     next();
   });
